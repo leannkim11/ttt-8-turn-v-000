0d8bf61f7645d1d2737d706d4006d1e1e00e0480
Leann Kim <leannminakim@gmail.com>
2018-03-08 05:10:28 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,24 @@
+def display_board
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def valid_move?(board,index)
+  if position_taken == false && index.between?(0,8)
+  true
+  end
+end
+
+def position_taken?(board,index)
+  if board[index] == "" || board[index] == " " || board[index] == nil
+    false
+  elsif board[index] == "X" || board[index] == "O"
+  end
+end
+
+def move
+  
+end
